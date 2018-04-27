718b1bd7cc6e4e798cdddfadfc93054f5d9bf626
Cody Heslep <codyheslep@gmail.com>
2018-04-27 10:21:52 -0500

    basic functionality

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const morgan = require('morgan');
+const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParse.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res, next) => {
   res.send('hello world');
 });
+
